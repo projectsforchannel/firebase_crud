bd221bc665725488e1ccb819fd2ad6f4f1acd450
projectsforchannel <projectsforchannel@gmail.com>
2021-05-31 16:05:50 +0530

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crud/add.dart';
+import 'package:firebase_crud/database.dart';
+import 'package:firebase_crud/view.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Database db;
+  List docs = [];
+  initialise() {
+    db = Database();
+    db.initiliase();
+    db.read().then((value) => {
+          setState(() {
+            docs = value;
+          })
+        });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initialise();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(56, 75, 49, 1.0),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(56, 75, 49, 1.0),
+        title: Text("Country"),
+      ),
+      body: ListView.builder(
+        itemCount: docs.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            margin: EdgeInsets.all(10),
+            child: ListTile(
+              onTap: () {
+                Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                View(country: docs[index], db: db)))
+                    .then((value) => {
+                          if (value != null) {initialise()}
+                        });
+              },
+              contentPadding: EdgeInsets.only(right: 30, left: 36),
+              title: Text(docs[index]['name']),
+              trailing: Text(docs[index]['code']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Add(db: db)))
+              .then((value) {
+            if (value != null) {
+              initialise();
+            }
+          });
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/view.dart
@@ -0,0 +1,104 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'database.dart';
+
+class View extends StatefulWidget {
+  View({Key key, this.country, this.db}) : super(key: key);
+  Map country;
+  Database db;
+  @override
+  _ViewState createState() => _ViewState();
+}
+
+class _ViewState extends State<View> {
+  TextEditingController nameController = new TextEditingController();
+  TextEditingController codeController = new TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    print(widget.country);
+    nameController.text = widget.country['name'];
+    codeController.text = widget.country['code'];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(56, 75, 49, 1.0),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(56, 75, 49, 1.0),
+        title: Text("Country View"),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                widget.db.delete(widget.country["id"]);
+                Navigator.pop(context, true);
+              })
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: inputDecoration("Country Name"),
+                controller: nameController,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: inputDecoration("Country Code"),
+                controller: codeController,
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        color: Colors.transparent,
+        child: BottomAppBar(
+          color: Colors.transparent,
+          child: RaisedButton(
+              color: Colors.black,
+              onPressed: () {
+                widget.db.update(widget.country['id'], nameController.text,
+                    codeController.text);
+                Navigator.pop(context, true);
+              },
+              child: Text(
+                "Save",
+                style: TextStyle(color: Colors.white),
+              )),
+        ),
+      ),
+    );
+  }
+
+  InputDecoration inputDecoration(String labelText) {
+    return InputDecoration(
+      focusColor: Colors.white,
+      labelStyle: TextStyle(color: Colors.white),
+      labelText: labelText,
+      fillColor: Colors.white,
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(25.0),
+        borderSide: BorderSide(color: Colors.white),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(25.0),
+        borderSide: BorderSide(
+          color: Colors.black,
+          width: 2.0,
+        ),
+      ),
+    );
+  }
+}
